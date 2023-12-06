ce64db93b215e579569cdf6f3278e74574034173
HemjalCF <abu.hemjal@connectedfinland.fi>
2023-12-07 01:56:33 +0200

    cut copy and paste function implemeted

--- notepad/mainwindow.cpp
+++ notepad/mainwindow.cpp
@@ -6,10 +6,78 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    this->setCentralWidget(ui->textEdit);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+
+void MainWindow::on_actionNew_triggered()
+{
+
+}
+
+
+void MainWindow::on_actionOpen_triggered()
+{
+
+}
+
+
+void MainWindow::on_actionSave_triggered()
+{
+
+}
+
+
+void MainWindow::on_actionSave_as_triggered()
+{
+
+}
+
+
+void MainWindow::on_actionCut_triggered()
+{
+    ui->textEdit->cut();
+
+}
+
+
+void MainWindow::on_actionCopy_triggered()
+{
+    ui->textEdit->copy();
+
+}
+
+
+void MainWindow::on_actionPaste_triggered()
+{
+    ui->textEdit->paste();
+
+}
+
+
+void MainWindow::on_actionRedo_triggered()
+{
+    ui->textEdit->redo();
+
+}
+
+void MainWindow::on_actionUndo_triggered()
+{
+    ui->textEdit->undo();
+
+}
+
+
+void MainWindow::on_actionAbout_Notepad_triggered()
+{
+
+}
+
+
+
 
--- notepad/mainwindow.hh
+++ notepad/mainwindow.hh
@@ -15,6 +15,27 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_actionNew_triggered();
+
+    void on_actionOpen_triggered();
+
+    void on_actionSave_triggered();
+
+    void on_actionSave_as_triggered();
+
+    void on_actionCut_triggered();
+
+    void on_actionCopy_triggered();
+
+    void on_actionPaste_triggered();
+
+    void on_actionRedo_triggered();
+
+    void on_actionAbout_Notepad_triggered();
+
+    void on_actionUndo_triggered();
+
 private:
     Ui::MainWindow *ui;
 };
