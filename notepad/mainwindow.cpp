6da3531446d2d0aff8d54371f8aa368a6557b4b7
HemjalCF <abu.hemjal@connectedfinland.fi>
2023-12-08 01:47:06 +0200

    save and save as method implemented

--- notepad/mainwindow.cpp
+++ notepad/mainwindow.cpp
@@ -47,11 +47,36 @@ void MainWindow::on_actionOpen_triggered()
 void MainWindow::on_actionSave_triggered()
 {
 
+    QFile file(_file_path);
+    if(!file.open(QFile::WriteOnly | QFile::Text)) {
+        QMessageBox::warning(this,"..","file not open");
+        return;
+    }
+
+    QTextStream out(&file);
+    QString text = ui->textEdit->toPlainText();
+    out<< text;
+    file.flush();
+    file.close();
+
 }
 
 
 void MainWindow::on_actionSave_as_triggered()
 {
+    QString file_name = QFileDialog::getSaveFileName(this,"Open the file");
+    QFile file(file_name);
+    _file_path= file_name;
+    if(!file.open(QFile::WriteOnly | QFile::Text)) {
+        QMessageBox::warning(this,"..","file not open");
+        return;
+    }
+
+    QTextStream out(&file);
+    QString text = ui->textEdit->toPlainText();
+    out<< text;
+    file.flush();
+    file.close();
 
 }
 
@@ -92,6 +117,12 @@ void MainWindow::on_actionUndo_triggered()
 
 void MainWindow::on_actionAbout_Notepad_triggered()
 {
+    QString about_text = "Notepad is a small and fast notetaking app for making notes, memos, or just any plain text content.\n Features: \n \
+                             * simple interface and easy to use \n\
+                             * no limits on note's length or number of notes \n\
+                             * creating and editing text notes\n\
+                             * importing or saving notes from txt files\n";
+    QMessageBox::about(this, "Notepad", about_text);
 
 }
 
